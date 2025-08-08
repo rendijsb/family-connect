21fa212f0c450ee76d56d83cb634cb17605f7a5b
rendijsberkis <rendijsberkis7@gmail.com>
2025-08-08 10:39:46 +0300

    fixed bugs

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.yourcompany.familyconnect',
   appName: 'Family Connect',
-  webDir: 'dist',
+  webDir: 'dist/browser',
   server: {
     androidScheme: 'https',
     iosScheme: 'capacitor'
