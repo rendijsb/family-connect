b70a2d738668f2931cc0e693fb5c740fe5e6770b
rendijsberkis <rendijsberkis7@gmail.com>
2025-09-02 14:49:35 +0300

    fixes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://46.62.158.78/api',
+  apiUrl: 'http://46.62.158.78/api',
   appName: 'Family Connect',
   version: '1.0.0',
   enableDebugMode: false
